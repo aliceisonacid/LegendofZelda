14d1c1d9cfdc61b0afd73dd51ba6607d5f39d213
Maris <69923858+aliceisonacid@users.noreply.github.com>
2023-11-29 14:29:21 -0500

    Updated linkscript.js

--- linkscript.js
+++ linkscript.js
@@ -1,37 +1,31 @@
-
 const link = document.querySelector('.link');
 const pipe = document.querySelector('.tree');
 
 const jump = () => { 
     link.classList.add('jump');
-    
 
     setTimeout(() => {
         link.classList.remove('jump');
-        
     }, 1000);
 };
 
+const loop = () => {
+    const pipePosition = pipe.offsetLeft;
+    const linkPosition = +window.getComputedStyle(link).bottom.replace('px');
 
-const loop = setInterval(() => {
+    console.log(linkPosition);
 
-const pipePosition = tree.offsetLeft;
-const linkPosition = +window.getComputedStyle(link).bottom.replace('px');
+    if (pipePosition < 120 && pipePosition > 0 && linkPosition < 80) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
 
-console.log(linkPosition);
+        link.style.animation = 'none';
+        link.style.bottom = `${linkPosition}px`;
+    }
 
-
-
-if (pipePosition < 120 && pipePosition > 0 && linkPosition < 80) {
-
-    pipe.style.animation = 'none' ;
-    pipe.style.left = `${treePosition}px`;
-
-    link.style.animation = 'none';
-    link.style.bottom = `${treePosition}px`;
+    requestAnimationFrame(loop);
 }
 
-}, 10)
+loop();
 
 document.addEventListener('click', jump);
-
